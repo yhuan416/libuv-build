7e08dfa978a1db4eafe17cbe982af511b680c8fb
yhuan416 <1290635392@qq.com>
2023-05-04 10:54:13 +0800

    feat: update

--- build_arm.sh
+++ build_arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 toolchain_file="../cross_cmake/arm.cmake"
-install_path="../install_arm"
+install_path="../install.arm"
 
 mkdir -p build && cd build
 cmake ../libuv -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${install_path}
