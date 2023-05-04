e16ad818e9ddca8b4663d7b2bc398cc260c1800b
yhuan416 <1290635392@qq.com>
2023-05-04 11:16:24 +0800

    feat: update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 toolchain_file="../cross_cmake/linux.cmake"
-install_path="../install"
+install_path="../install/libuv"
 
 mkdir -p build && cd build
 cmake ../libuv -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${install_path}
--- build_arm.sh
+++ build_arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 toolchain_file="../cross_cmake/arm.cmake"
-install_path="../install.arm"
+install_path="../install.arm/libuv"
 
 mkdir -p build && cd build
 cmake ../libuv -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${install_path}
