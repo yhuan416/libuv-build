182c7ae0607935bcdb77e3c2375a6a8651c878da
yhuan416 <1290635392@qq.com>
2023-05-04 10:51:31 +0800

    feat: add file

--- build_arm.sh
+++ build_arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-toolchain_file="../cross_cmake/linux.cmake"
-install_path="../install"
+toolchain_file="../cross_cmake/arm.cmake"
+install_path="../install_arm"
 
 mkdir -p build && cd build
 cmake ../libuv -DCMAKE_TOOLCHAIN_FILE=${toolchain_file} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${install_path}
